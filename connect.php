d43bf5338874ffd1037c76eee512df87dee74460
Jafar <you@example.com>
2020-01-04 11:40:38 +0300

    connect

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db= new PDO ("mysql:host=localhost;dbname=masraflar;charset=utf8", "masraflar", "");
+    $db= new PDO ("mysql:host=localhost;dbname=masraflar;charset=utf8", "masraflar", "12345");
 } catch (PDOException $e) {
    echo $e->getMessage();
 }
