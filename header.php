51ce957faa8c2cc013fcf134bd8a6c9257b1fa93
jraa <ceferelizade@gmail.com>
2020-02-09 20:28:29 +0300

    dnm

--- header.php
+++ header.php
@@ -26,7 +26,7 @@ $checkusers = $askusers->fetch(PDO::FETCH_ASSOC);
 
     <title>Masraflarım - Ücretsiz Masraflarını hesapla</title>
 
-    <!-- Bootstrap core CSS -->
+    <!-- Bootstrap core CSSs -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <!--external css-->
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
