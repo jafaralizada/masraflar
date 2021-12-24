9211745346ba025c600ef7859f775b65edd55d84
jafaralizada <ceferelizade@gmail.com>
2021-12-24 18:18:09 +0300

    son

--- income_add.php
+++ income_add.php
@@ -2,6 +2,9 @@
      <?php include "header.php"; ?>
      <?php include "sidebar.php";?>
   
+
+
+     
   <!--main content start-->
    <section id="main-content">
           <section class="wrapper site-min-height">
@@ -12,6 +15,19 @@
 
                                  <div class="form-group">
                                             <label>Gelir Miktari</label>
+                                            <?php 
+$askusers=$db->prepare("SELECT users_namesurname,users_id FROM users WHERE users_name=:users_name");
+$askusers->execute(array(
+'users_name' => $_SESSION['users_name']
+));
+
+foreach ($askusers as $row)
+{
+$kullanici_id= $row['users_id'] ;
+$timestamp = date("Y-m-d h:i:s");
+echo $timestamp;
+}
+        ?>
                                             <input class="form-control" name="income_amount" type="number" required>
                                      <p class="help-block">Gelir Miktari ekleyin.</p>
                                         </div>
@@ -28,12 +44,15 @@
           	</div>
             
               <?php
+
+
+
 if(isset($_POST["add"])){
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
-$sql = "INSERT INTO income (income_description, income_amount)
-VALUES ('".$_POST["income_description"]."','".$_POST["income_amount"]."')";
+$sql = "INSERT INTO income (income_description, income_amount, income_user_id, income_date)
+VALUES ('".$_POST["income_description"]."','".$_POST["income_amount"]."',$kullanici_id, ";
 
 if ($db->query($sql)) {
     echo "<script type= 'text/javascript'>alert('Sipariş Basariyla Eklendi!');</script>";
